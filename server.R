8df4f444abbe9d0ee9d72cc1ccb9f2def85e7385
rjhamilton <bobha@seanet.com>
2015-11-20 22:20:39 -0800

    added documentation

--- server.R
+++ server.R
@@ -1,13 +1,17 @@
-library(shiny)
+#library(shiny)
 
 data(sunspots)
+dfltYearRange <- c(1800, 1899)
+dfltPlotType <- "p"
+dfltBinSize <- 25
 
 shinyServer(
-	function(input, output) {
-		output$yearFrom <- renderPrint({input$yearRange[1]})
-		output$yearTo <- renderPrint({input$yearRange[2]})
-		output$plotType <- renderPrint({input$plotType})
-		output$binSize <- renderPrint({input$binSize})
+	function(input, output, session) {
+		observeEvent(input$reset_input, {
+			updateSliderInput(session, "yearRange", value=dfltYearRange)
+			updateRadioButtons(session, "plotType", selected=dfltPlotType)
+			updateSliderInput(session, "binSize", value=dfltBinSize)
+		})
 
 		output$tsPlot <- renderPlot({
 			t1 <- input$yearRange[1]
@@ -15,7 +19,7 @@ shinyServer(
 			pt <- input$plotType
 			bs <- input$binSize
 
-			y <- window(sunspots, start=t1, end=t2)
+			y <- suppressWarnings(window(sunspots, start=t1, end=t2))
 			y.sm <- window(filter(sunspots, rep(1, bs) / bs), start=t1, end=t2)
 
 			plot(y, type=pt, xlab="year", ylab="sunspot count")
--- ui.R
+++ ui.R
@@ -1,17 +1,26 @@
 library(shiny)
 
+data(sunspots)
+minYear <- as.integer(tsp(sunspots)[1])
+maxYear <- as.integer(tsp(sunspots)[2])
+dfltYearRange <- c(1800, 1899)
+dfltPlotType <- "p"
+dfltBinSize <- 25
+
 pageWithSidebar(
 	headerPanel("Sunspots"),
 	sidebarPanel(
-		sliderInput("yearRange", "yearFrom/To:", value = c(1944, 1976), min = 1749, max = 1984, step = 1, sep="", dragRange=TRUE),
-		radioButtons("plotType", "plotType:", c(points="p", lines="l", steps="s"), inline=TRUE),
-		sliderInput("binSize", "binSize (months):", value = 25, min = 1, max = 8 * 12 + 1, step = 2)
+		sliderInput("yearRange", "yearFrom/To window:", min=minYear, max=maxYear, step=1, sep="", dragRange=TRUE, value=dfltYearRange),
+		radioButtons("plotType", "raw dataset plotType:", c(points="p", lines="l", steps="s"), inline=TRUE, selected=dfltPlotType),
+		sliderInput("binSize", "smoothed dataset binSize (months):", min=1, max=8 * 12 + 1, step=2, value=dfltBinSize),
+		helpText(
+			"Select a range of years from the monthly sunspot dataset.",
+			"Drag either endpoint or drag the entire interval with your mouse.",
+			"Next, smooth the dataset by specifying binSize, i.e. the number of datapoints included in the moving average."),
+		actionButton("reset_input", "Reset")
+
 	),
 	mainPanel(
-		h4("yearFrom:"), verbatimTextOutput("yearFrom"),
-		h4("yearTo:"), verbatimTextOutput("yearTo"),
-		h4("plotType:"), verbatimTextOutput("plotType"),
-		h4("binSize:"), verbatimTextOutput("binSize"),
 		plotOutput("tsPlot")
 	)
 )
