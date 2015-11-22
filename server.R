30ff64d79bb5a9309854efdd1b777707b738f706
rjhamilton <bobha@seanet.com>
2015-11-22 11:17:08 -0800

    v1.1

--- server.R
+++ server.R
@@ -1,28 +1,43 @@
-library(shiny)
+####
+# sunspots Shiny App - server.R
+# Bob Hamilton, bobha@seanet.com
+#
 
-data(sunspots)
+data(sunspot.month)
+ss <- sunspot.month				## choose sunspots or sunspot.month
+
+minYear <- ceiling(tsp(ss)[1])		## first full year
+maxYear <- floor(tsp(ss)[2] - 11/12)	## last full year
+
+## condition default values
 dfltYearRange <- c(1800, 1899)
 dfltPlotType <- "p"
 dfltBinSize <- 25
 
+
 shinyServer(
 	function(input, output, session) {
+		## reset button
 		observeEvent(input$reset_input, {
 			updateSliderInput(session, "yearRange", value=dfltYearRange)
 			updateRadioButtons(session, "plotType", selected=dfltPlotType)
 			updateSliderInput(session, "binSize", value=dfltBinSize)
 		})
 
+		## time-series plot
 		output$tsPlot <- renderPlot({
 			t1 <- input$yearRange[1]
 			t2 <- input$yearRange[2] + 11/12
 			pt <- input$plotType
 			bs <- input$binSize
 
-			y <- suppressWarnings(window(sunspots, start=t1, end=t2))
-			y.sm <- window(filter(sunspots, rep(1, bs) / bs), start=t1, end=t2)
+			y <- window(ss, start=t1, end=t2)
+			y.sm <- window(filter(ss, rep(1, bs) / bs), start=t1, end=t2)
 
-			plot(y, type=pt, xlab="year", ylab="sunspot count")
+			title <- sprintf("%d years of sunspot activity, %d-%d", 
+				as.integer(t2 - t1 + 1), as.integer(t1), as.integer(t2))
+
+			plot(y, type=pt, cex=0.5, xlab="year", ylab="sunspot count", main=title)
 			lines(y.sm, col="red", lwd=2)
 		})
 	}
--- ui.R
+++ ui.R
@@ -1,26 +1,29 @@
-library(shiny)
+####
+# sunspots Shiny App - ui.R
+# Bob Hamilton, bobha@seanet.com
+#
 
-data(sunspots)
-minYear <- as.integer(tsp(sunspots)[1])
-maxYear <- as.integer(tsp(sunspots)[2])
-dfltYearRange <- c(1800, 1899)
-dfltPlotType <- "p"
-dfltBinSize <- 25
-
-pageWithSidebar(
-	headerPanel("Sunspots"),
-	sidebarPanel(
-		sliderInput("yearRange", "yearFrom/To window:", min=minYear, max=maxYear, step=1, sep="", dragRange=TRUE, value=dfltYearRange),
-		radioButtons("plotType", "raw dataset plotType:", c(points="p", lines="l", steps="s"), inline=TRUE, selected=dfltPlotType),
-		sliderInput("binSize", "smoothed dataset binSize (months):", min=1, max=8 * 12 + 1, step=2, value=dfltBinSize),
-		helpText(
-			"Select a range of years from the monthly sunspot dataset.",
-			"Drag either endpoint or drag the entire interval with your mouse.",
-			"Next, smooth the dataset by specifying binSize, i.e. the number of datapoints included in the moving average."),
-		actionButton("reset_input", "Reset")
-
-	),
-	mainPanel(
-		plotOutput("tsPlot")
+shinyUI(
+	pageWithSidebar(
+		headerPanel("Sunspots"),
+		sidebarPanel(
+			sliderInput("yearRange", "yearFrom/To window:", 
+				min=minYear, max=maxYear, step=1, sep="", dragRange=TRUE, value=dfltYearRange),
+			radioButtons("plotType", "raw dataset plotType:", 
+				c(points="p", lines="l", steps="s"), inline=TRUE, selected=dfltPlotType),
+			sliderInput("binSize", "smoothed dataset binSize (months):", 
+				min=1, max=8 * 12 + 1, step=2, value=dfltBinSize),
+			helpText(
+				"Select a range of years from the monthly sunspot activity dataset.",
+				"Drag either endpoint or drag the interval bar with your mouse.",
+				"Next, smooth the dataset by specifying binSize,",
+				"i.e. the number of datapoints included in the moving average."
+			),
+			actionButton("reset_input", "Reset")
+		),
+		mainPanel(
+			plotOutput("tsPlot")
+		)
 	)
 )
+
